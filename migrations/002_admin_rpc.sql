223274664c05a59f437a3525110638fc1fbb971e
gregojero1 <gregojero1@gmail.com>
2026-05-14 15:03:53 +0200

    Update 002_admin_rpc.sql

--- migrations/002_admin_rpc.sql
+++ migrations/002_admin_rpc.sql
@@ -1,1 +1,27 @@
+-- Fonction RPC pour vérifier le mot de passe admin
+-- À ajouter dans une migration Supabase
+
+create or replace function check_admin_password(input_password text)
+returns boolean
+language plpgsql
+security definer
+as $$
+declare
+  stored_hash text;
+begin
+  select password_hash into stored_hash
+  from admin_users
+  where username = 'admin'
+  limit 1;
+
+  if stored_hash is null then
+    return false;
+  end if;
+
+  return stored_hash = crypt(input_password, stored_hash);
+end;
+$$;
+
+-- Changer le mot de passe admin :
+-- update admin_users set password_hash = crypt('nouveau_mot_de_passe', gen_salt('bf')) where username = 'admin';
 
